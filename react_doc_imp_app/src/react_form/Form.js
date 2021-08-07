58237e3284b866ff3f4adeceafcb4d61f88b03f8
RumpaPaul <rumpakucse14@gmail.com>
2021-08-07 18:47:14 +0600

    Created common handle-change function

--- react_doc_imp_app/src/react_form/Form.js
+++ react_doc_imp_app/src/react_form/Form.js
@@ -4,43 +4,40 @@ const Form = () => {
     const [name, setName] = React.useState('');
     const [password, setPassword] = React.useState('');
 
-    function handleChangeName(event) {
-        console.log('handle change name');
-        setName(event.target.value);
-        event.preventDefault();
-    }
-
-    function handleChangePassword(event) {
-        console.log('handle change password');
-        setPassword(event.target.value);
-        event.preventDefault();
-    }
-
-    function handleSubmit(event){
+    function handleSubmit(event) {
         console.log('handle submit event');
-        console.log(name+' '+password);
+        console.log(name + ' ' + password);
         event.preventDefault();
+    }
+
+    function handleChange(event, elementName) {
+        event.preventDefault();
+        if (elementName === 'name') {
+            console.log('handle change name');
+            setName(event.target.value);
+        }
+        else if (elementName === 'password') {
+            console.log('handle change password');
+            setPassword(event.target.value);
+        }
     }
 
     return (
         <div>
             <h1>Login Form</h1>
-            {/* <span>{name}</span>
-            <br/>
-            <span>{password}</span> */}
             <div>
                 <label>
-                    Name: <input type='text' value={name || ''} placeholder='Enter name' onChange={handleChangeName} />
+                    Name: <input type='text' value={name || ''} placeholder='Enter name' onChange={(e) => handleChange(e, 'name')} />
                 </label>
                 <br />
                 <br />
                 <label>
-                    Password: <input type='password' value={password || ''} placeholder='Enter password' onChange={handleChangePassword} />
+                    Password: <input type='password' value={password || ''} placeholder='Enter password' onChange={(e) => handleChange(e, 'password')} />
                 </label>
                 <br />
                 <br />
                 <button onClick={handleSubmit}>Login</button>
-                </div>
+            </div>
         </div>
     )
 }
